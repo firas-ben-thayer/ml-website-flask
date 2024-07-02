335d162028c3a83a47153c130219efc4dbbc881f
Firas Ben Thayer <59771615+firas-ben-thayer@users.noreply.github.com>
2024-07-02 16:27:54 +0200

    first push
    Co-Authored-By: AwsEryani <159526805+awseryani@users.noreply.github.com>
    Co-Authored-By: Nima Saghi <164624152+nimasaghi@users.noreply.github.com>

---
+++ market/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'instance', 'market.db')}"
+app.config["SECRET_KEY"] = '61f249e58305f7ade79019ad'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+from market import routes # this needs to be here to execute the code after importing everything
---
+++ market/models.py
@@ -0,0 +1,45 @@
+from market import db, app, bcrypt, login_manager
+from flask_login import UserMixin # this contains predetermined calls that we need in login_manager along with login_manager check https://flask-login.readthedocs.io/en/latest/#how-it-works fpr more info
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User (db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(length=20), unique=True, nullable=False)
+    email_address = db.Column(db.String(length=80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+    
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f"{str(self.budget)[:-3]},{str(self.budget)[-3:]}$"
+        else:
+            return f"{self.budget}$"
+    
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+        
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+    
+class Item(db.Model):
+    id = price = db.Column(db.Integer(), primary_key = True, nullable=False)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return f'Item {self.name}'
+app.app_context().push() # This is needed when we want to create a new database when we run the code db.create_all() we need to push context first
